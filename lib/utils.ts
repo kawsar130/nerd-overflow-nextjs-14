1d26b407a8ada7642ed2a4d44b60555c4692696c
Kawsar Ahmed <kawsarahmed130@gmail.com>
2024-02-18 22:08:21 +0600

    Home page implemented

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,44 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getTimestamp = (createdAt: Date): string => {
+  const now = new Date();
+  const diff = now.getTime() - createdAt.getTime();
+
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(months / 12);
+
+  if (years > 1) {
+    return `${years} year${years === 1 ? "" : "s"} ago`;
+  } else if (months > 1) {
+    return `${months} month${months === 1 ? "" : "s"} ago`;
+  } else if (days > 1) {
+    return `${days} day${days === 1 ? "" : "s"} ago`;
+  } else if (hours > 1) {
+    return `${hours} hour${hours === 1 ? "" : "s"} ago`;
+  } else if (minutes > 1) {
+    return `${minutes} minute${minutes === 1 ? "" : "s"} ago`;
+  } else {
+    return `${seconds} second${seconds === 1 ? "" : "s"} ago`;
+  }
+};
+
+export const formatBigNumber = (number: number): string => {
+  if (number >= 1000000000) {
+    return (number / 1000000000).toFixed(1) + "B";
+  } else if (number >= 1000000) {
+    return (number / 1000000).toFixed(1) + "M";
+  } else if (number >= 1000) {
+    return (number / 1000).toFixed(1) + "K";
+  } else {
+    return number.toString();
+  }
+};
